dbff3bf3ad92b0b44f5043ea3caa530b8e9c6963
mfelipe94 <mfelipepadilla@gmail.com>
2024-07-12 00:01:03 -0700

    imp_test

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
           <a>Community</a>
           <a>Craft NFT</a>
 
-          <button id="connect-wallet" onClick={handleWallet} >{!account ? 'Connect Wallet' : account}</button>
+          <button id="connect-wallet">0xa943B664871933e28e2E5A818F9d0431d4571F79</button>
         </div>
       </div>
     );
