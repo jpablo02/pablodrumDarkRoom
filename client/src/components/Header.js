2bf48f14105e0dae8333c50d725634207a75a1ff
mfelipe94 <mfelipepadilla@gmail.com>
2024-07-22 09:40:14 -0700

    updated_MVP_2

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import react, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { useEthers, useEtherBalance } from "@usedapp/core";
+
+const Header = () => {
+
+    const {activateBrowserWallet, account} = useEthers();
+    const etherBalance = useEtherBalance(account);
+
+    const handleWallet = () => {
+      activateBrowserWallet();
+      // console.log(account); 
+      // account: wallet address
+    }
+
+    return (
+        <div id="header">
+        <Link to='/' id='logo'>NFT Room</Link>
+
+        <div id="link-containers">
+          <a>Start Hunting</a>
+          <a>Dark NFTs</a>
+          <a>Community</a>
+          <a>Craft NFT</a>
+
+          <button id="connect-wallet" onClick={handleWallet}>0xa943B664871933e28e2E5A818F9d0431d4571F79</button>
+        </div>
+      </div>
+    );
+}
+
+export default Header;
